fd212d93a22840ed783a32e7489dd097a305e03d
advojote <joseph.h.davis@missionary.org>
2024-11-06 14:19:13 -0800

    added js file, to alert contents of read file on page upon load

---
+++ script.js
@@ -0,0 +1,22 @@
+const reader = new FileReader();
+
+reader.onload = function(event) {
+    const fileContent = event.target.result;
+    // Do something with the file content
+    console.log(fileContent);
+  };
+
+  reader.onerror = function(event) {
+    console.error("Error reading file:", event.target.error);
+  };
+
+  let contents = reader.readAsText(file);
+  alert(contents);
+  
+//   // Attach the readFile function to an input element
+//   const video_container = document.getElementById("video container");
+//   let file = 
+//   video_container.addEventListener("change", (event) => {
+//     const file = event.target.files[0];
+//     readFile(file);
+//   });
